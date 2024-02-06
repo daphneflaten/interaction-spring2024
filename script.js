f10ba62cd16cd9cf284aa28717ee3aed9ee34efa
daphneflaten <flatd472@newschool.edu>
2024-02-06 18:45:00 -0500

    uwifwbef
    ksabs;dnla'n

--- script.js
+++
@@ -1,8 +0,0 @@
-function onButtonClick() {
-    alert('Yay you clicked me!');
-  }
-
-const button = document.querySelector('button');
-button.addEventListener('click', onButtonClick);
-
-console.log(lowerCaseString); // Output: Hello, I'm Daphne. I am a second year Communication Design student. I'm from orange county New York, and am a commuter student!
